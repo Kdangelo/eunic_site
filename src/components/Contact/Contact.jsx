2e43f90418c690e51500e5e90ca59604d91820e7
Kdangelo <kdangelof@gmail.com>
2025-03-09 16:39:42 -0300

    feat: contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,5 @@
 import "../../styles/Contact.css";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Col, Container, Row } from 'react-bootstrap';
 import ContactForm from './ContactForm';
 
 const Contact = () => {
@@ -9,8 +7,9 @@ const Contact = () => {
     <section>
       <Container fluid>
         <Row>
+          {/* Columna izquierda - Información de contacto */}
           <Col lg={5} className='order-2 order-lg-1 text-bg-primary p-5 contact-section'>
-            <div className='m-5 d-md-none d-lg-block'>
+            <div className='m-5 d-none d-lg-block'>
               <h1>
                 Contáctate con <strong className='text-warning'>Espacio Único</strong> 
               </h1>
@@ -36,16 +35,19 @@ const Contact = () => {
                 <iframe
                   src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3325.3514199343817!2d-70.55961031116958!3d-33.544244704643944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662d13938089d37%3A0xc36d7aeca5c7455e!2sSta.%20Amalia%203500%2C%20Depto%2033%20F%20Edificio%20F%2C%208290886%20La%20Florida%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses!2scl!4v1719968890769!5m2!1ses!2scl'
                   width='100%'
-                  height='100%'
+                  height='250'
                   style={{ border: 0 }}
-                  allowFullScreen=''
+                  allowFullScreen
                   loading='lazy'
                   referrerPolicy='no-referrer-when-downgrade'
                 ></iframe>
               </div>
             </div>
           </Col>
-          <ContactForm />
+          {/* Columna derecha - Formulario */}
+          <Col lg={7} className='order-1 order-lg-2 p-5'>
+            <ContactForm />
+          </Col>
         </Row>
       </Container>
     </section>
--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,9 +1,85 @@
+import "../../styles/ContactForm.css";
+import { Button, Col, Form, Row } from "react-bootstrap";
+
 const ContactForm = () => {
   return (
-    <div>
-      <h2>Contact Form</h2>
-      <p>This is a basic contact form.</p>
-    </div>
+    <>
+      <div className="mb-5 d-lg-none d-md-block">
+        <h1 className="text-center">
+          Contáctate con <strong className="text-warning">Espacio Único</strong>
+        </h1>
+      </div>
+      <Form className="mx-auto" style={{ maxWidth: "900px" }}>
+        <Row className="mb-3">
+          <Col md={6}>
+            <Form.Group controlId="formName">
+              <Form.Label>Nombre *</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Ingrese su Nombre"
+                required
+              />
+            </Form.Group>
+          </Col>
+
+          <Col md={6}>
+            <Form.Group controlId="formLastName">
+              <Form.Label>Apellido</Form.Label>
+              <Form.Control type="text" placeholder="Ingrese su Apellido" />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row className="mb-3">
+          <Col md={6}>
+            <Form.Group controlId="formEmail">
+              <Form.Label>Tu Email *</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Ingrese su Email"
+                required
+              />
+            </Form.Group>
+          </Col>
+
+          <Col md={6}>
+            <Form.Group controlId="formCompany">
+              <Form.Label>Compañía *</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Ingrese su Compañía"
+                required
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Form.Group className="mb-3" controlId="formAsunto">
+          <Form.Label>Asunto *</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ingrese el asunto del mensaje"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formMensaje">
+          <Form.Label>Mensaje *</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={4}
+            placeholder="Escribanos su mensaje"
+            required
+          />
+        </Form.Group>
+
+        <div className="d-grid">
+          <Button variant="warning" type="submit" size="lg">
+            Enviar Mensaje
+          </Button>
+        </div>
+      </Form>
+    </>
   );
 };
 
