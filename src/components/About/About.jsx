a51c9d2f6690455fef20ae5097665eeb5396f2da
Kdangelo <kdangelof@gmail.com>
2025-03-11 23:22:49 -0300

    ui: about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,48 +5,58 @@ const About = () => {
   return (
     <section className="about-section pt-5">
       <Container>
-        <Row>
-          <p className="text-white">SERVICIOS Y PRODUCTOS</p>
-          <h1 className="text-white">
-            Contamos con más de 17 años de{" "}
-            <span className="text-warning">experiencia</span> en diseño,
-            desarrollo, fabricación e instalación de muebles de cocina, closet,
-            vanitorios, muebles para oficina, quinchos, mobiliario escolar para{" "}
-            <sapn className="text-warning">
-              clientes Inmobiliarios, Particulares e Instituciones Públicas
-            </sapn>
-            .
-          </h1>
+        <Row className="mb-5">
+          <Col xs={12}>
+            <p className="text-white opacity-75 mb-5">NOSOTROS</p>
+            <h1 className="display-5 fw-bold text-white">
+              Contamos con más de 17 años de{" "}
+              <span className="fst-italic text-warning">experiencia</span> en
+              diseño, desarrollo, fabricación e instalación de muebles de
+              cocina, closet, vanitorios, muebles para oficina, quinchos,
+              mobiliario escolar para{" "}
+              <sapn className="fst-italic text-warning">
+                clientes Inmobiliarios, Particulares e Instituciones Públicas
+              </sapn>
+              .
+            </h1>
+          </Col>
         </Row>
-        <Row>
-          <Col>
+        <Row className="g-3 g-md-4 g-xl-5 mt-5">
+          <Col xs={12}>
             <h2 className="text-white">Aportamos valor.</h2>
           </Col>
-          <Col>
-            <Col>
-              <h3>¿Por qué elegirnos?</h3>
-            </Col>
-            <Col>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
-                corrupti corporis itaque officia quae esse cupiditate! Nam
-                accusantium dolores recusandae vero veniam, labore ab quasi qui!
-                Quod, impedit! Pariatur, iusto?
-              </p>
-            </Col>
+          <Col md={4}>
           </Col>
-          <Col>
-            <Col>
-              <h3>Ofrecemos</h3>
-            </Col>
-            <Col>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut
-                mollitia culpa voluptate repellendus cum ducimus laboriosam odio
-                dolor necessitatibus! Ratione animi quos temporibus esse nobis,
-                possimus soluta labore cupiditate fuga.
+          <Col md={4}>
+            <div className="h-100 p-4">
+              <h3 className="mb-3 text-warning">¿Por qué elegirnos?</h3>
+              <p className="mb-0 text-white">
+                Buscamos la excelencia en todo lo que hacemos y estamos
+                comprometidos a contribuir a la sociedad en donde actuamos,
+                privilegiando la excelencia, la integridad, la calidad y la
+                innovación en el diseño. Celebramos las buenas ideas, la
+                camaradería y el alto desempeño.
               </p>
-            </Col>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div className="h-100 p-4">
+              <h3 className="mb-3 text-warning">Ofrecemos</h3>
+              <p className="mb-0 text-white">
+                Esmerada atención, entrega a tiempo, muebles de alta calidad, y
+                precios competitivos, complementada por un alto grado de
+                rectitud en la interrelación con clientes, proveedores y
+                terceros interesados.
+              </p>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="how-we-work" md={12}>
+            <p className="text-white opacity-75 mb-5">ASÍ TRABAJAMOS</p>
+            <h3 className="text-white">Desarrollo de un proyecto</h3>
+          </Col>
+          <Col md={12}>
           </Col>
         </Row>
       </Container>
