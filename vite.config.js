91906717d5a76271cf615d766a4f5bfd0a245a83
kdangelo <kdangelof@gmail.com>
2025-04-03 18:40:50 -0300

    Fix image paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/eunic_site/' : '/', // Replace "eunic_site" with your repo name
+  base: command === 'build' ? '/eunic_site/' : '/', // Replace 'eunic_site' with your repo name
   build: {
     outDir: 'dist',
   },
