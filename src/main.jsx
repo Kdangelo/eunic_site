f5c60036fd06206732a2bfe32bf9d4e1daa6b05a
Kdangelo <kdangelof@gmail.com>
2025-04-06 13:30:22 -0400

    fix: add ScrollToTop component to improve mobile navigation

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import '../src/assets/styles/Global.scss';
 import './assets/scss/bootstrap.scss';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from '../src/components/common/CrollToTop/ScrollToTop.jsx';
 
 const basename = import.meta.env.MODE === 'production' ? '/eunic_site' : '/';
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
     <BrowserRouter basename={basename}>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
